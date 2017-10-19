e986eeb08eafb9921a4ec23d74e92a65418e9493
Mizuki Zenta <mizukisb@gmail.com>
2017-10-19 23:47:48 +1100

    'AddRandom'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,10 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    @post_count = Post.all.count
+    if params[:random]
+      @random_food = Post.find(params[:random])
+    end
   end
 
   # GET /posts/1
@@ -63,13 +67,13 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:image, :title, :description, :recipe, :duration, :user_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:image, :title, :description, :recipe, :duration, :user_id)
+  end
 end
